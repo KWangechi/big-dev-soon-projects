b34b5878d74d34d6463122422ce0e81cdfd5f9d2
KWangechi <rachel.karanja@strathmore.edu>
2024-01-11 23:25:39 +0300

    project:initialized a calculator app in Quasar

---
+++ calculator-app/src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout class="bg-blue-4">
+    v
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    // const leftDrawerOpen = ref(false)
+
+    return {};
+  },
+});
+</script>
---
+++ calculator-app/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="container text-center">
+    <h4 class="q-mt-xl">Quasar Calculator App</h4>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
+<style scoped></style>
